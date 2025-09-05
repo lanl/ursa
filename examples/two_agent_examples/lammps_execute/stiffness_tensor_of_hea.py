686e8a29377cf662dc07b380211f82d8ed320c1e
Rahul Somasundaram <rsomasundaram@lanl.gov>
2025-09-05 15:34:05 -0600

    Added another example for the LAMMPS agent

---
+++ examples/two_agent_examples/lammps_execute/stiffness_tensor_of_hea.py
@@ -0,0 +1,48 @@
+from ursa.agents import LammpsAgent
+from ursa.agents import ExecutionAgent
+
+from langchain_core.messages import HumanMessage
+from langchain_openai import ChatOpenAI
+
+model = "gpt-5"
+
+llm = ChatOpenAI(model      = model,
+                timeout     = None,
+                max_retries =  2
+                )
+
+
+wf = LammpsAgent(
+    llm = llm,
+    max_potentials=5,
+    max_fix_attempts=10,      
+    mpi_procs=8,
+    lammps_cmd="lmp_mpi",
+    mpirun_cmd="mpirun",
+)
+
+high_level="Carry out a LAMMPS simulation of the high entropy alloy Co-Cr-Fe-Mn-Ni to determine its stiffness tensor."
+elements=["Co","Cr","Fe","Mn","Ni"]
+
+final_lammps_state = wf.run(high_level,elements)
+
+if final_lammps_state.get("run_returncode") == 0:
+
+    print ("\nNow handing things off to execution agent.....")
+
+    executor = ExecutionAgent(llm=llm)
+    exe_plan = f"""
+    You are part of a larger scientific workflow whose purpose is to accomplish this task: {high_level}
+    
+    A LAMMPS simulation has been done and the output is located here: ../log.lammps
+    
+    Summarize the contents of this file in a markdown document. Include a plot, if relevent.
+    """
+    
+    init = {"messages": [HumanMessage(content=exe_plan)]}
+    
+    final_results = executor.action.invoke(init, {"recursion_limit": 10000})
+    
+    for x in final_results["messages"]:
+        print(x.content)
+
