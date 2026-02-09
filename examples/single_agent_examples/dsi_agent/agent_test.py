3c9fa1b94e205bd2e436aaa56f3cdac1c6aba6f2
Pascal Grosset <pascalgrosset@lanl.gov>
2026-02-09 14:56:41 -0600

    removed globals

--- examples/single_agent_examples/dsi_agent/agent_test.py
+++ examples/single_agent_examples/dsi_agent/agent_test.py
@@ -8,21 +8,22 @@ from pathlib import Path
 # Get the data
 current_file = Path(__file__).resolve()
 current_dir = current_file.parent
+run_path = os.getcwd()
 
-dataset_path = current_dir / "data/oceans_11/ocean_11_datasets.db"
+dataset_path = str( current_dir / "data/oceans_11/ocean_11_datasets.db" )
 print(dataset_path)
 
 model = ChatOpenAI( model="gpt-5.1", max_tokens=100000, timeout=None, max_retries=2)
 
 workspace = "dsi_agent_example"
 os.makedirs(workspace, exist_ok=True)
-rdb_path = Path(workspace) / "websearcher_checkpoint.db"
+rdb_path = Path(workspace) / "dsi_agent_checkpoint.db"
 rdb_path.parent.mkdir(parents=True, exist_ok=True)
 rconn = sqlite3.connect(str(rdb_path), check_same_thread=False)
 dsiagent_checkpointer = SqliteSaver(rconn)
 
 
-ai = DSIAgent(llm=model, db_index_name=dataset_path, output_mode="console", checkpointer=dsiagent_checkpointer)
+ai = DSIAgent(llm=model, database_path=dataset_path, output_mode="console", checkpointer=dsiagent_checkpointer, run_path=run_path)
 
 print("\nQuery: Tell me about the datasets you have.")
 ai.ask("Tell me about the datasets you have.")
